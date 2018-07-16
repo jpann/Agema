fe39f479c5a6d57305011146e3f1ba2763c2a91e
Jay Cummins <jay.cummins@worldviewsolutions.com>
2018-07-16 08:12:44 -0400

    Added additional data types.

--- scripts/db/mssql/select_csharp_props.sql
+++ scripts/db/mssql/select_csharp_props.sql
@@ -4,13 +4,16 @@ CONCAT('public ',
 CASE 
 	WHEN DATA_TYPE IN ('nvarchar','varchar') THEN 'string'
 	WHEN DATA_TYPE IN ('smallint') THEN 'short'
+	WHEN DATA_TYPE IN ('bigint') THEN 'long'
 	WHEN DATA_TYPE IN ('int') THEN 'int'
-	WHEN DATA_TYPE IN ('tinyint') THEN 'byte'
-	
+	WHEN DATA_TYPE IN ('smallint') THEN 'int'
+	WHEN DATA_TYPE IN ('tinyint') THEN 'byte'	
+	WHEN DATA_TYPE IN ('bit') THEN 'boolean'
+	WHEN DATA_TYPE IN ('datetime','datetime2') THEN 'DateTime'	
 	ELSE 'string'
 END
 ,' ',COLUMN_NAME,' { get; set; }')
 ,*
 
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'v_WorkflowStep';
+WHERE TABLE_NAME = '';
