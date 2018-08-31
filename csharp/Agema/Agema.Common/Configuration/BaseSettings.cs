3acce56f33ff6e40f75f77b9d7e8c26d12f03cfe
Jay Cummins <jay.cummins@worldviewsolutions.com>
2018-08-31 08:33:37 -0400

    Added a .NET Standard library for common methods.

---
+++ csharp/Agema/Agema.Common/Configuration/BaseSettings.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Configuration;
+using System.Reflection;
+using Common.Logging;
+
+namespace Agema.Common.Configuration
+{
+    /// <summary>
+    ///     A base settings class
+    /// </summary>
+    public class BaseSettings
+    {
+        /// <summary>
+        ///     The Log (Common.Logging)
+        /// </summary>
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        /// <summary>
+        ///     Gets the program data path for application.  Many classes that inherit BaseSettings will have a
+        ///     SetProgramDataPath() method.
+        /// </summary>
+        /// <value>
+        ///     The program data path.
+        /// </value>
+        public static string ProgramDataPath { get; set; }
+
+        protected static T Get<T>(string key)
+        {
+            return Get(key, false, default(T));
+        }
+
+        protected static T Get<T>(string key, bool allowNull, T defaultValue)
+        {
+            var value = ConfigurationManager.AppSettings[key];
+
+            if (string.IsNullOrEmpty(value))
+            {
+                if (!allowNull)
+                    throw new ConfigurationErrorsException($"Missing configuration setting \"{key}\"");
+
+                return defaultValue;
+            }
+
+            return (T) Convert.ChangeType(value, typeof(T));
+        }
+
+        /// <summary>
+        ///     Sets the connection string.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        protected static void SetConnectionString(string key, string value)
+        {
+            var settings = ConfigurationManager.ConnectionStrings[key];
+
+            if (Log.IsDebugEnabled)
+                foreach (var connectionString in ConfigurationManager.ConnectionStrings)
+                {
+                    var connectionStringSetting = connectionString as ConnectionStringSettings;
+
+                    if (connectionStringSetting != null)
+                        Log.Debug(
+                            $"{connectionStringSetting.Name}: {connectionStringSetting.ConnectionString} ({connectionStringSetting?.CurrentConfiguration?.FilePath})");
+                }
+
+            if (settings == null)
+            {
+                ConfigurationManager.ConnectionStrings.Add(new ConnectionStringSettings(key, value));
+            }
+            else
+            {
+                var fi = typeof(ConfigurationElement).GetField("_bReadOnly",
+                    BindingFlags.Instance | BindingFlags.NonPublic);
+
+                if (fi != null)
+                {
+                    fi.SetValue(settings, false);
+
+                    settings.ConnectionString = value;
+                }
+            }
+        }
+    }
+}
---
+++ csharp/Agema/Agema.Common/Diagnostics/ILog.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Agema.Common.Diagnostics
+{
+    /// <summary>
+    ///     Borrowed from NLog, until Common.Logging is built for .NET Standard
+    /// </summary>
+    public interface ILog
+    {
+        /// <summary>
+        ///     Records trace info if enabled.  Accepts an optional operation that returns a string message to save processing if
+        ///     trace logging is disabled. Ignore the other parameters, they are received by reflection.
+        /// </summary>
+        void Trace(Func<string> message = null, [CallerMemberName] string memberName = null,
+            [CallerFilePath] string filePath = null, [CallerLineNumber] int lineNumber = 0);
+
+        /// <summary>
+        ///     Records debug info if enabled.  Accepts an optional operation that returns a string message to save processing if
+        ///     debug logging is disabled. Ignore the other parameters, they are received by reflection.
+        /// </summary>
+        void Debug(Func<string> message = null, [CallerMemberName] string memberName = null,
+            [CallerFilePath] string filePath = null, [CallerLineNumber] int lineNumber = 0);
+
+        /// <summary>
+        ///     Records info if enabled.  Accepts an optional operation that returns a string message to save processing if info
+        ///     logging is disabled. Ignore the other parameters, they are received by reflection.
+        /// </summary>
+        void Info(Func<string> message = null, [CallerMemberName] string memberName = null,
+            [CallerFilePath] string filePath = null, [CallerLineNumber] int lineNumber = 0);
+
+        /// <summary>
+        ///     Records warn info if enabled.  Accepts an operation that returns a string message to save processing if warn
+        ///     logging is disabled. Ignore the other parameters, they are received by reflection.
+        /// </summary>
+        void Warn(Func<string> message, Exception ex = null, [CallerMemberName] string memberName = null,
+            [CallerFilePath] string filePath = null, [CallerLineNumber] int lineNumber = 0);
+
+        /// <summary>
+        ///     Records warn info if enabled for an exception. Ignore the parameters, they are received by reflection.
+        /// </summary>
+        void Warn(Exception ex, [CallerMemberName] string memberName = null, [CallerFilePath] string filePath = null,
+            [CallerLineNumber] int lineNumber = 0);
+
+        /// <summary>
+        ///     Records error info if enabled.  Accepts an operation that returns a string message to save processing if error
+        ///     logging is disabled. Ignore the other parameters, they are received by reflection.
+        /// </summary>
+        void Error(Func<string> message, Exception ex = null, [CallerMemberName] string memberName = null,
+            [CallerFilePath] string filePath = null, [CallerLineNumber] int lineNumber = 0);
+
+        /// <summary>
+        ///     Records error info if enabled for an exception.  Ignore the parameters, they are received by reflection.
+        /// </summary>
+        void Error(Exception ex, [CallerMemberName] string memberName = null, [CallerFilePath] string filePath = null,
+            [CallerLineNumber] int lineNumber = 0);
+
+        /// <summary>
+        ///     Records fatal info if enabled.  Accepts an operation that returns a string message to save processing if fatal
+        ///     logging is disabled. Ignore the other parameters, they are received by reflection.
+        /// </summary>
+        void Fatal(Func<string> message, Exception ex, [CallerMemberName] string memberName = null,
+            [CallerFilePath] string filePath = null, [CallerLineNumber] int lineNumber = 0);
+
+        /// <summary>
+        ///     Records fatal info if enabled for an exception.  Ignore the parameters, they are received by reflection.
+        /// </summary>
+        void Fatal(Exception ex, [CallerMemberName] string memberName = null, [CallerFilePath] string filePath = null,
+            [CallerLineNumber] int lineNumber = 0);
+    }
+}
