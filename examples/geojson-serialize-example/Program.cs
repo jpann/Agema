5285e5ccfee77ceb1299e4e5bbde214033423088
Jay Cummins <jay.cummins@worldviewsolutions.com>
2018-08-31 08:55:31 -0400

    Added geojson-serialize example (for https://gis.stackexchange.com/questions/234268/geojson-net-serializing-to-valid-geojson-file)

---
+++ examples/geojson-serialize-example/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Agema.Common;
+using GeoJSON.Net.Geometry;
+using Newtonsoft.Json;
+
+namespace geojson_serialize_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var points = new List<Point>
+            {
+                new Point(new Position(52.370725881211314, 4.889259338378906)),
+                new Point(new Position(52.3711451105601, 4.895267486572266)),
+                new Point(new Position(52.36931095278263, 4.892091751098633)),
+                new Point(new Position(52.370725881211314, 4.889259338378906))
+            };
+
+            var multiPoint = new MultiPoint(points);
+
+
+            var actualJson = JsonConvert.SerializeObject(multiPoint);
+            // ReSharper disable once UseFormatSpecifierInInterpolation
+            var outputFilePath = Path.Combine(TempFileHelper.GetTemporaryDirectory(),
+                $"{Guid.NewGuid().ToString("N")}.json");
+
+            
+            File.WriteAllText(outputFilePath, actualJson);
+
+            Console.WriteLine($"GeoJson Geometry Written To: {outputFilePath}");
+        }
+    }
+}
---
+++ examples/geojson-serialize-example/TempFileHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Common.Logging;
+
+namespace Agema.Common
+{
+    /// <summary>
+    ///     Helper methods for creating Temporary files and directories
+    /// </summary>
+    public static class TempFileHelper
+    {
+        /// <summary>
+        ///     The Log (Common.Logging)
+        /// </summary>
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+
+        /// <summary>
+        ///     Gets the assembly application data directory directory (e.g. C:\Users\{{user}}\AppData\Roaming\{{assemblyname}})
+        /// </summary>
+        /// <returns></returns>
+        public static string GetAssemblyAppDataDirectory()
+        {
+            var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+            var assemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+
+            var assemblyAppData = Path.Combine(appDataPath, assemblyName);
+
+            if (!Directory.Exists(assemblyAppData))
+            {
+                Directory.CreateDirectory(assemblyAppData);
+
+                Log.Info("Create directory: {assemblyAppData}");
+            }
+
+            return assemblyAppData;
+        }
+
+        public static string GetTemporaryDirectory()
+        {
+            var tempDir = Path.Combine(GetAssemblyAppDataDirectory(), @"tmp");
+
+            if (!Directory.Exists(tempDir))
+            {
+                Directory.CreateDirectory(tempDir);
+
+                Log.Info("Create directory: {tempDir}");
+            }
+
+            return tempDir;
+        }
+    }
+}
